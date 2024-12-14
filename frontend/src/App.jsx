bec4033dc04d4f39d4bee3bf0f819a22710d91a6
niceman1234man <yihunietarekegn18@gmail.com>
2024-12-14 00:59:05 -0800

    frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Footer from './components/Layouts/Footer'
+import Navbar from './components/Layouts/Navbar'
+import Landing from './components/Layouts/Landing'
+
+function App() {
+  return (
+    <>
+    <Navbar/>
+    <Landing/>
+    <Footer/>
+    </>
+  )
+}
+
+export default App
