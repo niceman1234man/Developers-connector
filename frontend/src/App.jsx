d180973b6684921a13746be5406e807b557d4cf2
niceman1234man <yihunietarekegn18@gmail.com>
2024-12-15 22:04:34 -0800

    app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,21 @@ import React from 'react'
 import Footer from './components/Layouts/Footer'
 import Navbar from './components/Layouts/Navbar'
 import Landing from './components/Layouts/Landing'
+import { Routes,Route  } from 'react-router-dom'
+import Register from './Auth/Register'
+import Login from './Auth/Login'
 
 function App() {
   return (
     <>
     <Navbar/>
-    <Landing/>
+    <Routes>
+   
+    <Route path='/' element={<Landing/>} />
+    <Route path='/register' element={<Register/>} />
+    <Route path='/login' element={<Login/>} />
+   
+    </Routes>
     <Footer/>
     </>
   )
