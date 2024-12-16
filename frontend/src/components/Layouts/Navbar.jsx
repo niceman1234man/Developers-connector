10d6e2f213bb4c60d15354cc26117fdc93fed0f6
niceman1234man <yihunietarekegn18@gmail.com>
2024-12-15 22:02:04 -0800

    Navbar

--- frontend/src/components/Layouts/Navbar.jsx
+++ frontend/src/components/Layouts/Navbar.jsx
@@ -1,19 +1,19 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
 function Navbar() {
   return (
     <div className='bg-gray-900'>
         <div className='flex justify-evenly mx-auto items-center text-white'>
             <div >
                 <ul className='flex items-center'>
-                    <li className='p-2'>DevConnector</li>
-                    <li className='p-2'>Developer</li>
+                    <Link className='p-2' to='/'>DevConnector</Link>
+                    <Link className='p-2' to='/'>Developer</Link>
                 </ul>
             </div>
             <div>
             <ul className='flex mx-auto items-center '>
-                    <li className='p-2'>SignUp</li>
-                    <li className='p-2'>Login</li>
+                    <Link className='p-2' to='/register'>SignUp</Link>
+                    <Link className='p-2' to='/login'>Login</Link>
                 </ul>
             </div>
         </div>
